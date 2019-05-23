3f9105e5bb1a894c9bf4e2f1ad99300760c3d036
mymell2003 <mymell2003@gmail.com>
2019-05-23 14:19:30 +0800

    Update LCD1602_I2C.ts

--- LCD1602_I2C.ts
+++ LCD1602_I2C.ts
@@ -5,7 +5,7 @@
 */
 
 /**
- * I2C LCD1602 液晶软件包
+ * I2C LCD1602 液晶套裝軟體
  */
 //% weight=20 color=#0fbc11 icon="▀"
 namespace I2C_LCD1602 {
@@ -42,7 +42,7 @@ namespace I2C_LCD1602 {
         set(d << 4)
     }
 
-    // 自动识别I2C地址
+    // 自動識別I2C位址
     function AutoAddr() {
         let k = true
         let addr = 0x20
@@ -71,8 +71,8 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 初始化 LCD, 设置 I2C 地址。根据芯片不同地址有两种，PCF8574 是 39，PCF8574A 是 63
-     * 地址设置为0 代表自动识别
+     * 初始化 LCD, 設置 I2C 位址。根據晶片不同位址有兩種，PCF8574 是 39，PCF8574A 是 63
+     * 位址設置為0 代表自動識別
      * @param address is i2c address for LCD, eg: 0, 39, 63
      */
     //% blockId="I2C_LCD1620_SET_ADDRESS" block="初始化液晶，I2C 地址 %addr"
@@ -96,12 +96,12 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 在液晶的指定位置显示数字
+     * 在液晶的指定位置顯示數位
      * @param n is number will be show, eg: 10, 100, 200
      * @param x is LCD column position, eg: 0
      * @param y is LCD row position, eg: 0
      */
-    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="显示数字 %n|位置 x %x|y %y"
+    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="顯示數位 %n|位置 x %x|y %y"
     //% weight=90 blockGap=8
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
@@ -112,12 +112,12 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 在液晶的指定位置显示字符串
+     * 在液晶的指定位置顯示字串
      * @param s is string will be show, eg: "Hello"
      * @param x is LCD column position, [0 - 15], eg: 0
      * @param y is LCD row position, [0 - 1], eg: 0
      */
-    //% blockId="I2C_LCD1620_SHOW_STRING" block="显示字符串 %s|位置 x %x|y %y"
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="顯示字串 %s|位置 x %x|y %y"
     //% weight=90 blockGap=8
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
@@ -138,9 +138,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 打开液晶显示
+     * 打開液晶顯示
      */
-    //% blockId="I2C_LCD1620_ON" block="打开液晶"
+    //% blockId="I2C_LCD1620_ON" block="打開液晶"
     //% weight=81 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function on(): void {
@@ -148,9 +148,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 关闭液晶显示
+     * 關閉液晶顯示
      */
-    //% blockId="I2C_LCD1620_OFF" block="关闭液晶"
+    //% blockId="I2C_LCD1620_OFF" block="關閉液晶"
     //% weight=80 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function off(): void {
@@ -158,9 +158,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 清除液晶上显示的内容
+     * 清除液晶上顯示的內容
      */
-    //% blockId="I2C_LCD1620_CLEAR" block="清除液晶显示内容"
+    //% blockId="I2C_LCD1620_CLEAR" block="清除液晶顯示內容"
     //% weight=85 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function clear(): void {
@@ -168,9 +168,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 打开液晶的背光
+     * 打開液晶的背光
      */
-    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="打开液晶背光"
+    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="打開液晶背光"
     //% weight=71 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function BacklightOn(): void {
@@ -179,9 +179,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 关闭液晶的背光
+     * 關閉液晶的背光
      */
-    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="关闭液晶背光"
+    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="關閉液晶背光"
     //% weight=70 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function BacklightOff(): void {
@@ -190,9 +190,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 屏幕向左移动
+     * 螢幕向左移動
      */
-    //% blockId="I2C_LCD1620_SHL" block="屏幕左移"
+    //% blockId="I2C_LCD1620_SHL" block="螢幕左移"
     //% weight=61 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function shl(): void {
@@ -200,9 +200,9 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * 屏幕向右移动
+     * 螢幕向右移動
      */
-    //% blockId="I2C_LCD1620_SHR" block="屏幕右移"
+    //% blockId="I2C_LCD1620_SHR" block="螢幕右移"
     //% weight=60 blockGap=8
     //% parts=LCD1602_I2C trackArgs=0
     export function shr(): void {
